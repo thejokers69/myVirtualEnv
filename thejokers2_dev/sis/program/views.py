0457e767884306bf3301576cf7cf6e8a279673c1
Mohamed Lakssir <thejokkers69@gmail.com>
2024-04-01 22:16:16 +0000

    Added Using Urls and Views(1.define urlconfs,2.create view ,  )

--- thejokers2_dev/sis/program/views.py
+++ thejokers2_dev/sis/program/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from .models import Program
+from django.urls import reverse_lazy
 
-# Create your views here.
+# Define views here
+
+def prog_home_view(request):
+    # Define the view logic here
+    pass
+
+def prog_edit_success(request):
+    # Define the view logic here
+    pass
+
+def prog_delete_success(request):
+    # Define the view logic here
+    pass
+
+class ProgramListView(ListView):
+    model = Program
+    template_name = 'program/prog_list.html'
+
+class ProgramCreateView(CreateView):
+    model = Program
+    template_name = 'program/prog_edit_form.html'
+    success_url = reverse_lazy('prog-create-success')
+
+class ProgramUpdateView(UpdateView):
+    model = Program
+    template_name = 'program/prog_edit_form.html'
+    success_url = reverse_lazy('prog-edit-success')
+
+class ProgramDeleteView(DeleteView):
+    model = Program
+    template_name = 'program/prog_confirm_delete.html'
+    success_url = reverse_lazy('prog-delete-success')
+
+class ProgramDetailView(DetailView):
+    model = Program
+    template_name = 'program/prog_detail.html'
