26e291809f3fda04846440b5c8acfd1a8baa63b8
thejokers69 <thejokkers69@gmail.com>
2024-05-24 18:32:32 +0100

    Modified from colleg_carpool/carpool/templates

--- college_carpool/carpool/urls.py
+++ college_carpool/carpool/urls.py
@@ -1,10 +1,9 @@
+# carpool/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('profile/', views.profile, name='profile'),
-    path('create_ride/', views.create_ride, name='create_ride'),
+    path('create/', views.create_ride, name='create_ride'),
+    path('search/', views.search_ride, name='search_ride'),
 ]
